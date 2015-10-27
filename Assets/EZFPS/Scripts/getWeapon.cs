e85b9fb6314924228949d491f73c72ece934ac4c
YongMin <bumbermin@naver.com>
2015-10-28 01:18:47 +0900

    Make_Basic_Anim

---
+++ Assets/EZFPS/Scripts/getWeapon.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class getWeapon : MonoBehaviour {
+	
+	
+	
+	public static bool Gun1,Gun2 = false; 
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+		//		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	
+	void OnTriggerEnter(Collider other) {
+		
+		
+		//		Debug.Log ("other.GetComponentn : "+other.gameObject);
+		
+		
+		if (other.gameObject.tag == "Gun1") {
+			
+			Debug.Log ("other.gameObject : "+other.gameObject);
+			
+			Destroy (other.gameObject);
+			Gun1 = true;
+			
+			
+			
+		}
+		
+		
+		
+		if (other.gameObject.tag == "Gun2") {
+			Destroy (other.gameObject);
+			Gun2 = true;
+			
+			Debug.Log ("other.gameObject : "+other.gameObject);
+			
+		}
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
--- Assets/EZFPS/Scripts/roomManager.cs
+++ Assets/EZFPS/Scripts/roomManager.cs
@@ -3,30 +3,30 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class roomManager : Photon.MonoBehaviour {
-
+	
 	public string roomName;
 	public string playerName;
 	public string currentVersion = "0.1";
 	public string team1 = "Red";
 	public string team2 = "Blue";
 	public string currentTeam;
-
+	
 	public bool isConnected = false;
-
+	
 	public GameObject uiParent;
 	public GameObject gameUIParent;
 	public GameObject player1;
 	public GameObject player2;
-
+	
 	public GameObject mainCam;
-
+	
 	public Transform spawn1;
 	public Transform spawn2;
-
+	
 	public InputField roomField;
 	public InputField nameField;
 	public Text serverList;
-
+	
 	void Awake(){
 		PhotonNetwork.ConnectUsingSettings (currentVersion);
 		uiParent.SetActive (false);
@@ -34,87 +34,99 @@ public class roomManager : Photon.MonoBehaviour {
 		nameField.text = PlayerPrefs.GetString ("playerName");
 		roomField.text = "Room " + Random.Range (1,999);
 	}
-
+	
 	void Update(){
 		//GameObject otherCams = GameObject.Find ("Main Camera");
 		//otherCams.SetActive (false);
 		roomName = roomField.text;
 		playerName = nameField.text;
 		serverList.text = PhotonNetwork.countOfPlayers + " users are online in " + PhotonNetwork.countOfRooms + " rooms.";
-
+		
 		if (Input.GetKeyUp (KeyCode.Return)) {
 			spawnPlayer();
 		}
-
+		
 	}
-
+	
 	void OnJoinedLobby(){
 		uiParent.SetActive (true);
 		isConnected = true;
-
+		
 	}
-
+	
 	public void Join(){
 		setName ();
 		PhotonNetwork.JoinRoom (roomName);
 	}
-
+	
 	public void Create(){
 		setName ();
 		//PhotonNetwork.CreateRoom (roomName);
 		PhotonNetwork.JoinOrCreateRoom(roomName, null, null);
-
+		
 	}
-
+	
 	public void joinRandom(){
 		PhotonNetwork.JoinRandomRoom ();
 	}
-
+	
 	void OnJoinedRoom(){
 		mainCam.SetActive (true);
 		uiParent.SetActive (false);
 		currentTeam = team1;
 		gameUIParent.SetActive (true);
 	}
-
+	
 	void OnCreatedRoom(){
 		OnJoinedRoom ();
 	}
-
+	
 	public void setName(){
 		PhotonNetwork.playerName = playerName;
 		PlayerPrefs.SetString ("playerName", playerName);
 	}
-
+	
 	public void chooseRed(){
 		currentTeam = team1;
 	}
 	public void chooseBlue(){
 		currentTeam = team2;
 	}
-
+	
 	public void spawnPlayer(){
-			if (currentTeam == team1) {
-				gameUIParent.SetActive (false);
-				GameObject player = PhotonNetwork.Instantiate (player1.name, spawn1.position, spawn1.rotation, 0) as GameObject;
-				player.transform.FindChild("Main Camera").gameObject.SetActive(true);
-				((MonoBehaviour)player.GetComponent ("networkController")).enabled = true;	
-				((MonoBehaviour)player.GetComponent ("MouseLook")).enabled = true;	
 		
-			}
-			if (currentTeam == team2) {
-				gameUIParent.SetActive (false);
-				GameObject player = PhotonNetwork.Instantiate (player2.name, spawn2.position, spawn2.rotation, 0) as GameObject;
-				((MonoBehaviour)player.GetComponent ("networkController")).enabled = true;	
-				((MonoBehaviour)player.GetComponent ("MouseLook")).enabled = true;	
-				player.transform.FindChild("Main Camera").gameObject.SetActive(true);
-			}
-
-			mainCam.SetActive (false);
+		
+		//Red Player
+		if (currentTeam == team1) {
+			gameUIParent.SetActive (false);
+			GameObject player = PhotonNetwork.Instantiate (player1.name, spawn1.position, spawn1.rotation, 0) as GameObject;
+			player.transform.FindChild("Main Camera").gameObject.SetActive(true);
+			//				player.transform.FindChild("Graphics").gameObject.SetActive(true);
+			((MonoBehaviour)player.GetComponent ("networkController")).enabled = true;	
+			((MonoBehaviour)player.GetComponent ("MouseLook")).enabled = true;	
+			
+			
 		}
-
-
-
+		
+		//Blue Player
+		if (currentTeam == team2) {
+			
+			Debug.Log("spawnPlayer(), team2");
+			gameUIParent.SetActive (false);
+			GameObject player = PhotonNetwork.Instantiate (player2.name, spawn2.position, spawn2.rotation, 0) as GameObject;
+			player.transform.FindChild("Main Camera").gameObject.SetActive(true);
+			
+			player.transform.FindChild("Graphics").gameObject.SetActive(true);
+			((MonoBehaviour)player.GetComponent ("networkController")).enabled = true;	
+			((MonoBehaviour)player.GetComponent ("MouseLook")).enabled = true;	
+			
+		}
+		
+		mainCam.SetActive (false);
+	}
+	
+	
+	
 	
 	
 }
