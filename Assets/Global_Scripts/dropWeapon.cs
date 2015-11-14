596a43854247922357603cd41556e4d0213075b8
YongMin <bumbermin@naver.com>
2015-11-14 16:01:41 +0900

    mistake_revert

--- Assets/Global_Scripts/dropWeapon.cs
+++ Assets/Global_Scripts/dropWeapon.cs
@@ -21,6 +21,20 @@ public class dropWeapon : MonoBehaviour {
 			Debug.Log("Drop It!");
 			animManager.Play (drop_d.name);
 		}
+
+
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		
+		
+//		if (transform.position.x <= 3) {
+			// Move the object forward along its z axis 1 unit/second.
+//			transform.Translate (new Vector3 (3, 0, 0) * Time.deltaTime);
+//		}
+
 		
 	}
 
