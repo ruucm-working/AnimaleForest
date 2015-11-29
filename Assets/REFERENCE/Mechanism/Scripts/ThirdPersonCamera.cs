cff2d28a31dfbd2f06d392bb0d2b130f896ed554
YongMin <bumbermin@naver.com>
2015-11-29 18:43:23 +0900

    Hierarchy Arrange

--- Assets/REFERENCE/Mechanism/Scripts/ThirdPersonCamera.cs
+++ Assets/REFERENCE/Mechanism/Scripts/ThirdPersonCamera.cs
@@ -7,12 +7,13 @@ using System.Collections;
 public class ThirdPersonCamera : MonoBehaviour
 {
 	public float distanceAway;			
-	public float distanceUp;			
+	public float distanceUp;		
+	public float distanceRot;
 	public float smooth;				// how smooth the camera movement is
 		
 	private Vector3 m_TargetPosition;		// the position the camera is trying to be in)
 	
-	Transform follow;
+	Transform follow, camera;
 	
 	void Start(){
 		follow = GameObject.FindWithTag ("Player").transform;	
@@ -21,10 +22,22 @@ public class ThirdPersonCamera : MonoBehaviour
 	void LateUpdate ()
 	{
 		// setting the target position to be the correct offset from the 
-		m_TargetPosition = follow.position + Vector3.up * distanceUp - follow.forward * distanceAway;
+		m_TargetPosition = follow.position+ new Vector3(0.13f,0.005f,0.04f)+ new Vector3(0.14f,0.004f,0.02f) + Vector3.up * distanceUp - follow.forward * distanceAway ;
+
+
+		 
+		 
+		Debug.Log ("follow.forward : "+follow.forward); 
+		Debug.Log ("follow.up : "+follow.up); 
+		Debug.Log ("follow.rotation : "+follow.rotation); 
+
+
 		
 		// making a smooth transition between it's current position and the position it wants to be in
 		transform.position = Vector3.Lerp(transform.position, m_TargetPosition, Time.deltaTime * smooth);
+
+
+
 		
 		// make sure the camera is looking the right way!
 		transform.LookAt(follow);
