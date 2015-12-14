ff7aaa63e8157290b41cdc9043495ee136d4b378
YongMin <bumbermin@naver.com>
2015-12-15 08:59:58 +0900

    Add Start GUI

---
+++ Assets/Global_Scripts/UIButtonHover.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIButtonHover : MonoBehaviour {
+
+	public Sprite red;
+	// Image img;
+
+	// Use this for initialization
+	void Start () {
+
+
+		// img =  GetComponent<Image>();
+
+		// img.sprite = red;
+
+
+
+	}
+	void Update () {
+
+	}
+	
+
+
+}
